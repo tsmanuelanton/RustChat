2dab4cad76237f1f909a5625c5a807f214857d0c
UO276213 <uo276213@uniovi.es>
2023-05-07 23:01:25 +0200

    Create a client or server by arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use std::thread;
-
+use std::{env, thread};
 mod server;
 mod client;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let host_type = args.get(1);
+    if host_type != None {
 
-    thread::spawn(move || server::start_server());
-    client::connect_to("127.0.0.1:3000");
+        if matches!(host_type.unwrap().trim(), "server"){
+            println!("Creating a new server");
+            server::start_server();
+        }else {
+            println!("Attempting to a connect to 127.0.0.1:3000");
+            client::connect_to("127.0.0.1:3000");
+        }
+
+    }else {
+        println!("Missing host type argument. Add server o client argument.");
+    }
 }
