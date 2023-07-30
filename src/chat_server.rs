0975726839ab8e3f439c1ec1b390b8ac7729d8da
UO276213 <uo276213@uniovi.es>
2023-07-30 14:06:54 +0200

    not panic when user disconnects

--- src/chat_server.rs
+++ src/chat_server.rs
@@ -5,11 +5,12 @@ use std::time::SystemTime;
 use futures::{SinkExt, StreamExt};
 use hyper::upgrade::Upgraded;
 use serde::{Deserialize, Serialize};
-use tokio::sync::{mpsc, Mutex};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use tokio::sync::{mpsc, Mutex};
 use tokio_tungstenite::WebSocketStream;
 use tungstenite::Message;
 
+#[derive(Clone)]
 pub struct Client {
     id: String,
     addr: SocketAddr,
@@ -34,13 +35,18 @@ pub type ServerStateSafe = Arc<Mutex<ServerState>>;
 /*
 Adds a new client to the clients list. Reads the stream and send a broadcast to other clients.
  */
-pub async fn handle_new_connection(ws: WebSocketStream<Upgraded>, addr: SocketAddr, server_state: &ServerStateSafe) {
+pub async fn handle_new_connection(
+    ws: WebSocketStream<Upgraded>,
+    addr: SocketAddr,
+    server_state: &ServerStateSafe,
+) {
     let mut state = server_state.lock().await;
     let client_id = state.clients.len().to_string();
     println!("New client {client_id} from ip {addr}");
 
     // create async channel for communicating between tasks
-    let (tx, mut rx): (UnboundedSender<Message>, UnboundedReceiver<Message>) = mpsc::unbounded_channel();
+    let (tx, mut rx): (UnboundedSender<Message>, UnboundedReceiver<Message>) =
+        mpsc::unbounded_channel();
 
     // Adds a new client to the clients list
     let client = Client {
@@ -55,10 +61,30 @@ pub async fn handle_new_connection(ws: WebSocketStream<Upgraded>, addr: SocketAd
     // split socket for simultaneous reading and writing
     let (mut sink, mut read) = ws.split();
 
+    let client_id_cloned = client_id.clone();
+    let state = server_state.clone();
     // task for sending the received msg from channel to the ws
     tokio::spawn(async move {
+        let state = state.clone();
         while let Some(msg) = rx.recv().await {
-            sink.send(msg).await.unwrap();
+            match sink.send(msg).await {
+                Ok(_) => (),
+                Err(e) => match e.into() {
+                    tokio_tungstenite::tungstenite::Error::ConnectionClosed => {
+                        println!("Client {client_id_cloned} disconnected");
+                        let mut server_state = state.lock().await;
+                        if let Some(index) = server_state
+                            .clients
+                            .iter()
+                            .position(|value| value.id == client_id_cloned)
+                        {
+                            server_state.clients.swap_remove(index);
+                        }
+                        break;
+                    }
+                    _ => {}
+                },
+            }
         }
         sink.close().await.unwrap();
     });
@@ -84,9 +110,7 @@ async fn broadcast_msg(msg: Msg, server_state: &ServerStateSafe) {
     let state = server_state.lock().await;
 
     for client in &state.clients {
-        // if msg.client_id != client.id {
         let msg_json = Message::Text(serde_json::to_string(&msg).unwrap());
         client.sender.send(msg_json).unwrap();
-        // }
     }
 }
